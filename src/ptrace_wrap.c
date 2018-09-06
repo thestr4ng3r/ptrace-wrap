b6a45cddcaf5c6ff3407b6c185d00bf8e29b3daa
Florian Märkl <info@florianmaerkl.de>
2018-09-06 12:17:36 +0200

    Add license headers

--- src/ptrace_wrap.c
+++ src/ptrace_wrap.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of ptrace-wrap.
+ *
+ * ptrace-wrap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ptrace-wrap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ptrace-wrap.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <ptrace_wrap.h>
 
